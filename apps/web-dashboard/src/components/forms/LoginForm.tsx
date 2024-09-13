14750d6e13ad9a1ec3a98df2ace962c25560d06a
Xsromovsky <samuel14sromovsky@gmail.com>
2024-09-14 01:17:04 +0200

    adding shadcn components

--- apps/web-dashboard/src/components/forms/LoginForm.tsx
+++ apps/web-dashboard/src/components/forms/LoginForm.tsx
@@ -2,46 +2,82 @@ import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import LoginButton from './LoginButton';
 import { twMerge } from 'tailwind-merge';
 import classNames from 'classnames';
-
+import * as Tabs from '@radix-ui/react-tabs';
+import { Button } from '@/src/components/ui/Button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/src/components/ui/Form';
+import { Input } from '@/src/components/ui/Input';
 
 const Inputs = z.object({
-  email: z.string().email(),
+  email: z.string().email({ message: 'Invalid email address' }),
   password: z.string(),
 });
 
 type LoginForm = z.infer<typeof Inputs>;
 
 const LoginForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<LoginForm>({
+  const form = useForm<LoginForm>({
     resolver: zodResolver(Inputs),
   });
 
-  const inputClass = twMerge(classNames('bg-third p-1 rounded-lg text-white'))
+  const inputClass = twMerge(
+    classNames(
+      'bg-primary p-1 border border-third rounded-lg text-white focus:ring-2 focus:ring-white'
+    )
+  );
 
   const onSubmit: SubmitHandler<LoginForm> = (data) => {
     console.log(data);
-    
-  }
+  };
 
   return (
     <div className="flex bg-secondary w-1/6 justify-center items-center rounded-lg">
-        <div className='w-full h-full flex flex-col items-center m-3'>
-
-      <h1 className='text-white p-1 text-[30px] font-bold'>Sign in</h1>
-        <form className='flex flex-col w-full space-y-3 ' onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" {...register("email")} placeholder='example@mail.com' className={inputClass} />
-            <input type="password" {...register('password')} placeholder='password' className={inputClass}/>
-            <LoginButton label='Login' type='submit' className={'p-1 rounded-lg bg-third hover:bg-accent'}/>
-        </form>
-        </div>
-      
+      <div className="w-full h-full flex flex-col items-center  m-3">
+        <h1 className="text-white p-1 text-[30px] font-bold">Sign in</h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='w-full space-y-2'>
+            <FormField
+              name="email"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel className='px-1'>Email</FormLabel>
+                    <FormControl>
+                      <Input type="text" className={inputClass}/>
+                    </FormControl>
+                  </FormItem>
+                  
+                );
+              }}
+            />
+            <FormField
+              name="password"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel className='px-1'>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" className={inputClass}/>
+                    </FormControl>
+                  </FormItem>
+                  
+                );
+              }}
+            />
+            <span className='px-1'>Forgot password?</span>
+            <Button variant={"default"} className='hover:bg-third w-full'>Sign in</Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 };
